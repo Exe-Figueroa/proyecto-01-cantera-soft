6fa03f9ef98c8a5e4369f21e623ffc71abcf9a88
Exe-Figueroa <facundofigueroa789@gmail.com>
2023-02-08 10:05:03 -0300

    Resuelto el d

--- algoritmos.js
+++ algoritmos.js
@@ -22,3 +22,31 @@ var tijera=3
         }
     }
 juego(piedra, tijera)
+//7)d_
+var MarcasDeAutos=[
+    {marca: "Nissan", model: "Frontier"},
+    {marca: "Ferrari", model: "488"},
+    {marca: "Ford", model: "Falcon"},
+    {marca: "Chevrolet", model: "Super Sport 250"},
+    {marca: "Peugeot", model:"504"}
+    ];
+    var MarcaModelo=MarcasDeAutos.map(function(MarcaYmodelo){
+        console.log(`Marca: ${MarcaYmodelo.marca} Modelo: ${MarcaYmodelo.model}`)
+    })
+
+function LlamarMarca_o_Modelo(marcaYmodelo) {
+    if(marcaYmodelo=="Marcas"){
+        var marcas=MarcasDeAutos.map(function(laMarca){
+            return laMarca.marca});
+            return marcas
+    } else if(marcaYmodelo=="Modelos"){
+        var model=MarcasDeAutos.map(function(modelo){
+            return modelo.model})
+            return model
+    } else if(marcaYmodelo=="Marca y Modelo") {
+    return MarcaModelo
+    }
+}
+LlamarMarca_o_Modelo("Marca y Modelo");
+LlamarMarca_o_Modelo("Marcas");
+LlamarMarca_o_Modelo("Modelos");
